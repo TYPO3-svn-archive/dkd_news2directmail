9f1b481435c324fe5dbd086df2148e1eb0a47b26
ivankartolo <ivankartolo@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-11-25 10:39:34 +0000

    init import 0.1.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dkd_news2directmail/trunk@26945 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ cli/conf.php
@@ -0,0 +1,7 @@
+<?php
+/** conf.php,v 1.1 2006/11/23 17:17:40 dkd-kartolo Exp */
+// DO NOT REMOVE OR CHANGE THESE 3 LINES:
+define('TYPO3_MOD_PATH', '../typo3conf/ext/dkd_news2directmail/cli/');
+$BACK_PATH='../../../../typo3/';
+$MCONF['name'] = '_CLI_dkd_news2direct';
+?>
---
+++ mod1/conf.php
@@ -0,0 +1,14 @@
+<?php
+
+	// DO NOT REMOVE OR CHANGE THESE 3 LINES:
+define('TYPO3_MOD_PATH', '../typo3conf/ext/dkd_news2directmail/mod1/');
+$BACK_PATH='../../../../typo3/';
+$MCONF['name']='web_txdkdnews2directmailM1';
+
+	
+$MCONF['access']='user,group';
+$MCONF['script']='index.php';
+
+$MLANG['default']['tabs_images']['tab'] = 'moduleicon.gif';
+$MLANG['default']['ll_ref']='LLL:EXT:dkd_news2directmail/mod1/locallang_mod.xml';
+?>
