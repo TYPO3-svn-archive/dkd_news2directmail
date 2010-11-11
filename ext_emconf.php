c29ac01604e7b2d9ea83fe4bc5a2d011ff907430
ivankartolo <ivankartolo@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-11-11 10:08:44 +0000

    Release 1.0.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dkd_news2directmail/trunk@40067 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "dkd_news2directmail"
+# Extension Manager/Repository config file for ext "dkd_news2directmail".
 #
-# Auto generated 29-09-2007 17:44
+# Auto generated 11-11-2010 11:03
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'd.k.d Internet Service GmbH',
-	'version' => '0.1.0',
+	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
 			'tt_news' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:22:{s:9:"ChangeLog";s:4:"1f4b";s:10:"README.txt";s:4:"a4cb";s:21:"ext_conf_template.txt";s:4:"6e58";s:12:"ext_icon.gif";s:4:"b831";s:14:"ext_tables.php";s:4:"04a8";s:14:"ext_tables.sql";s:4:"3b01";s:16:"locallang_db.xml";s:4:"1772";s:31:"cli/class.ext_mod_web_dmail.php";s:4:"529f";s:37:"cli/class.ext_tx_directmail_dmail.php";s:4:"46cc";s:30:"cli/cli_dkdnews2directmail.php";s:4:"a97c";s:12:"cli/conf.php";s:4:"7354";s:14:"doc/manual.sxw";s:4:"4bfa";s:19:"doc/wizard_form.dat";s:4:"c93d";s:20:"doc/wizard_form.html";s:4:"a8fd";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"ea2f";s:14:"mod1/index.php";s:4:"37a2";s:18:"mod1/locallang.xml";s:4:"05a5";s:22:"mod1/locallang_mod.xml";s:4:"dcfe";s:19:"mod1/moduleicon.gif";s:4:"b831";s:45:"static/tt_news_single_plaintext/constants.txt";s:4:"25ca";s:41:"static/tt_news_single_plaintext/setup.txt";s:4:"2bdc";}',
+	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"f85f";s:10:"README.txt";s:4:"a4cb";s:12:"ext_icon.gif";s:4:"b831";s:14:"ext_tables.php";s:4:"04a8";s:14:"ext_tables.sql";s:4:"9ff2";s:16:"locallang_db.xml";s:4:"1772";s:37:"cli/class.ext_tx_directmail_dmail.php";s:4:"ce9b";s:30:"cli/cli_dkdnews2directmail.php";s:4:"2f76";s:12:"cli/conf.php";s:4:"7354";s:14:"doc/manual.sxw";s:4:"e427";s:19:"doc/wizard_form.dat";s:4:"c93d";s:20:"doc/wizard_form.html";s:4:"a8fd";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"ea2f";s:14:"mod1/index.php";s:4:"eaa3";s:18:"mod1/locallang.xml";s:4:"05a5";s:22:"mod1/locallang_mod.xml";s:4:"dcfe";s:19:"mod1/moduleicon.gif";s:4:"b831";s:45:"static/tt_news_single_plaintext/constants.txt";s:4:"25ca";s:41:"static/tt_news_single_plaintext/setup.txt";s:4:"2bdc";}',
 	'suggests' => array(
 	),
 );
